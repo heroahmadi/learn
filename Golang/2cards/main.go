8e932a1f6e6937fd685b2c7d096d9c85687be6f1
heroahmadi <hero.lionel@gmail.com>
2023-03-23 11:08:00 +0700

    learn/Golang: learn syntax
    learn variable declaration, array & slices, and custom type

--- Golang/2cards/main.go
+++ Golang/2cards/main.go
@@ -3,6 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of spades"
+	variables()
+	arrayAndSlices()
+	usingCustomType()
+}
+
+func variables() {
+	// var card string = "Ace of spades"
+	// alternative variable initialization above:
+	card := "Ace of spades"
+	// assign new value after initialization
+	card = "Five of diamonds"
+	// assign new value from function
+	card = newCard()
+
 	fmt.Println(card)
+}
+
+func arrayAndSlices() {
+	// Array: static (fixed) size
+	// Slice: dynamic size
+
+	// initializing slice
+	cards := []string{"Ace of diamonds", newCard()}
+	fmt.Println(cards)
+
+	// looping through the element, i = zero based index
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func usingCustomType() {
+	customTypeCards := deck{"Ace of diamonds", newCard()}
+	customTypeCards.print()
+}
+
+func newCard() string {
+	return "Five of diamonds"
 }
