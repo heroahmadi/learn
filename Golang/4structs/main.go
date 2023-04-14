3eedb3b823f6d189c4b0f4d2983044949e8c8a88
heroahmadi <hero.lionel@gmail.com>
2023-04-14 13:33:24 +0700

    learn structs

--- Golang/4structs/main.go
+++ Golang/4structs/main.go
@@ -2,9 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	// contact   contactInfo
+
+	// this means we create property named contactInfo of type contactInfo
+	contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
 
 func main() {
@@ -12,12 +29,26 @@ func main() {
 }
 
 func structCreation() {
-	alex := person{"Alex", "Anderson"}
-	alex2 := person{firstName: "Alex", lastName: "Anderson"}
+	// creating struct without the property name
+	alex := person{"Alex", "Anderson", contactInfo{"john@doe.com", 1900}}
+
+	// creating struct with property name
+	alex2 := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email:   "john@doe.com",
+			zipCode: 9000,
+		}}
 
 	fmt.Println(alex, alex2)
+	alex.print()
+	alex2.print()
 
 	// default value of string in struct is a zero value (empty string, 0, or false), not nil
 	var emptyStruct person
 	fmt.Printf("%+v", emptyStruct)
+
+	person.updateName("New first name")
+	person.print()
 }
