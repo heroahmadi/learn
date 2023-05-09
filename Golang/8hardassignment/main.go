7b66b9a7292973f820bda9742f3b475bc72e8e6a
heroahmadi <hero.lionel@gmail.com>
2023-05-09 14:55:00 +0700

    assignment hard mode

---
+++ Golang/8hardassignment/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("ERROR!", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, file)
+}
