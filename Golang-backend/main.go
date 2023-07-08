103c02c86eb7dd19d918c17067793a76857587bc
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2023-07-08 13:08:03 +0700

    routing with switch case

--- Golang-backend/main.go
+++ Golang-backend/main.go
@@ -7,6 +7,14 @@ import (
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world")
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "Hello world")
+	case "/test":
+		fmt.Fprint(w, "This is a test")
+	default:
+		fmt.Fprint(w, "Not found!")
+	}
 }
 
 func main() {
