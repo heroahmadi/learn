0a13dacfb9e3538908ece87747b4ae31cfebec67
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2023-10-11 16:07:05 +0700

    create post page form with formik

--- Fullstack/MyERN/client/src/pages/CreatePost.js
+++ Fullstack/MyERN/client/src/pages/CreatePost.js
@@ -1,9 +1,23 @@
 import React from "react";
+import {Formik, Form, Field} from 'formik';
 
 function CreatePost() {
     return (
-        <div className="App">
+        <div className="createPostPage">
+            <Formik>
+                <Form className="formContainer">
+                    <label>Title: </label>
+                    <Field id="inputCreatePost" name="title" placeHolder="(Ex: My Vacation)"></Field>
 
+                    <label>Text: </label>
+                    <Field id="inputCreatePost" name="text" placeHolder="(Ex: My Nice Vacation)"></Field>
+
+                    <label>Username: </label>
+                    <Field id="inputCreatePost" name="username" placeHolder="(Ex: John Doe)"></Field>
+
+                    <button type="submit">Create post</button>
+                </Form>
+            </Formik>
         </div>
     )
 }
