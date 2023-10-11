fe2be0409256c1ed79829112ebefe151b3efe84f
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2023-10-11 16:23:52 +0700

    complete form for submit new post

--- Fullstack/MyERN/client/src/pages/CreatePost.js
+++ Fullstack/MyERN/client/src/pages/CreatePost.js
@@ -1,19 +1,42 @@
 import React from "react";
-import {Formik, Form, Field} from 'formik';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import axios from "axios";
 
 function CreatePost() {
+    const initialValues = {
+        title: "",
+        text: "",
+        username: ""
+    };
+
+    const onSubmit = (data) => {
+        axios.post("http://localhost:3001", data).then((response) => {
+            console.log("POSTED!");
+        });
+    }
+
+    const validationScheme = Yup.object().shape({
+        title: Yup.string().required("Title cannot be empty!"),
+        text: Yup.string().required(),
+        username: Yup.string().min(3).max(25).required()
+    });
+
     return (
         <div className="createPostPage">
-            <Formik>
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationScheme}>
                 <Form className="formContainer">
                     <label>Title: </label>
-                    <Field id="inputCreatePost" name="title" placeHolder="(Ex: My Vacation)"></Field>
+                    <ErrorMessage name="title" />
+                    <Field autocomplete="off" id="inputCreatePost" name="title" placeHolder="(Ex: My Vacation)"></Field>
 
                     <label>Text: </label>
-                    <Field id="inputCreatePost" name="text" placeHolder="(Ex: My Nice Vacation)"></Field>
+                    <ErrorMessage name="text" />
+                    <Field autocomplete="off" id="inputCreatePost" name="text" placeHolder="(Ex: My Nice Vacation)"></Field>
 
                     <label>Username: </label>
-                    <Field id="inputCreatePost" name="username" placeHolder="(Ex: John Doe)"></Field>
+                    <ErrorMessage name="username" />
+                    <Field autocomplete="off" id="inputCreatePost" name="username" placeHolder="(Ex: John Doe)"></Field>
 
                     <button type="submit">Create post</button>
                 </Form>
