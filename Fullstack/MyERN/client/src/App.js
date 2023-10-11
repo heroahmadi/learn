c9d84ab6655d0e6f002449880208e4b37b65cf35
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2023-10-11 12:35:52 +0700

    Connect client to server

--- Fullstack/MyERN/client/src/App.js
+++ Fullstack/MyERN/client/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/").then((resp) => {
+      setListOfPosts(resp.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      
+      {listOfPosts.map((val, key) => {
+        return (
+          <div> {val.title} </div>
+        )
+      })}
     </div>
   );
 }
