1c678b779b09b38937b35bff134b0c3144767c3c
Hero Akbar Ahmadi <hero.lionel@gmail.com>
2023-10-15 21:06:16 +0700

    implement client side login

--- Fullstack/MyERN/client/src/pages/Login.js
+++ Fullstack/MyERN/client/src/pages/Login.js
@@ -1,9 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, {useState} from "react"; 
 
 function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
+    const login = () => {
+        const data = {username: username, password: password};
+        axios.post(`http://localhost:3001/auth/login`, data).then((resp) => {
+            console.log(data);
+        });
+    };
     return (
-        <div>Login
+        <div>
+            <label>Username: </label>
+            <input 
+                onChange={(event) => {setUsername(event.target.value)}}
+                value={username}
+            />
+
+            <label>Password: </label>
+            <input 
+                type="password"
+                onChange={(event) => {setPassword(event.target.value)}}
+                value={password}
+            />
+
+            <button onClick={login}>Login</button>
         </div>
     )
 }
